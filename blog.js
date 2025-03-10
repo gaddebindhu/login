023bfaecea77e045c37e82dfe6986a39836429c7
gaddebindhu <gaddebindhu@gmail.com>
2025-03-10 16:00:41 +0530

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,8 @@
+// Optional JavaScript to toggle "Read More" button functionality
+document.querySelectorAll('.read-more').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Redirecting to full post...');
+        // You can replace this with actual redirection logic, for example:
+        // window.location.href = '/full-post-url';
+    });
+});
